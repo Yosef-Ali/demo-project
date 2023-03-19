a106c56943f850a55935e740efae824ead0a407d
Codux <codux@codux.com>
2023-03-19 22:53:12 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { LandingPage } from './components/landing-page/landing-page';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <LandingPage />
+        </div>
+    );
+}
+export default App;
